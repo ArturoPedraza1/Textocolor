a7c0617fe9207fdba80af64ef6998320c8b81433
ArturoPedraza1 <125940774+ArturoPedraza1@users.noreply.github.com>
2026-05-17 20:33:45 -0600

    Add files via upload

---
+++ Combo.java
@@ -0,0 +1,76 @@
+// Pedraza Cruz José Arturo
+
+package com.example.funciones;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+public class Combo extends Activity {
+
+    Spinner comboNC;
+    EditText et1;
+
+    @Override
+    protected void onCreate(Bundle b) {
+        super.onCreate(b);
+
+        setContentView(R.layout.vista);
+
+        comboNC = findViewById(R.id.comboopciones);
+        et1 = findViewById(R.id.textocambio);
+
+        String[] colores = {"Rojo", "Verde", "Azul"};
+
+        ArrayAdapter<String> ad = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_spinner_item,
+                colores
+        );
+
+        ad.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        comboNC.setAdapter(ad);
+
+        comboNC.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+
+                cambiarColor(position);
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+    }
+
+    public void cambiarColor(int posicion) {
+
+        switch (posicion) {
+
+            case 0:
+                et1.setTextColor(Color.RED);
+                break;
+
+            case 1:
+                et1.setTextColor(Color.GREEN);
+                break;
+
+            case 2:
+                et1.setTextColor(Color.BLUE);
+                break;
+        }
+
+    }
+
+}
